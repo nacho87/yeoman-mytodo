3c9ea422b79be54a007e9532a642adf06328498a
Nacho Decima <jidecima@gmail.com>
2014-07-06 19:54:18 -0300

    [First commit]
      - Test Yeoman

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name mytodoApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the mytodoApp
+ */
+angular.module('mytodoApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.todos = [
+      'Item 1',
+      'Item 2',
+      'Item 3',
+      'Item 4'
+    ];
+  });
